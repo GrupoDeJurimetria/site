b713d64ad84717ddf415c010820fab30e23ae0a5
guijun13 <guilhermejun13.grillo@gmail.com>
2021-10-14 23:19:54 -0300

    fix: most of pages render

---
+++ index.js
@@ -0,0 +1,46 @@
+function slide1() {
+  document.getElementById('id').src = './imagens/A.png';
+  t1 = setTimeout('slide2()', 8000);
+  document.getElementById('aId').href = '/Video.html';
+  n = 0;
+}
+
+function slide2() {
+  document.getElementById('id').src = './imagens/B.png';
+  t2 = setTimeout('slide3()', 8000);
+  document.getElementById('aId').href = '/Project.html';
+  n = 1;
+}
+
+function slide3() {
+  document.getElementById('id').src = './imagens/C.png';
+  t3 = setTimeout('slide1()', 8000);
+  document.getElementById('aId').href = 'link3.html';
+  n = 2;
+}
+
+function mais() {
+  if (n == 0) {
+    clearTimeout(t1);
+    slide2();
+  } else if (n == 1) {
+    clearTimeout(t2);
+    slide3();
+  } else if (n == 2) {
+    clearTimeout(t3);
+    slide1();
+  }
+}
+
+function menos() {
+  if (n == 0) {
+    clearTimeout(t1);
+    slide3();
+  } else if (n == 1) {
+    clearTimeout(t2);
+    slide1();
+  } else if (n == 2) {
+    clearTimeout(t3);
+    slide2();
+  }
+}
